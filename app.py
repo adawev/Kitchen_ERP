eca309334ed1dd23bc844375e736729e8a503eea
Diyor Adashev <adashevdiyor443@gmail.com>
2025-11-18 15:33:00 +0500

    menu

---
+++ app.py
@@ -0,0 +1,14 @@
+kitchen_menu = {
+    "1": {"name": "Osh", "price": 25000},
+    "2": {"name": "Manti", "price": 30000},
+    "3": {"name": "Lag'mon", "price": 22000},
+    "4": {"name": "Shurva", "price": 18000},
+    "5": {"name": "Chuchvara", "price": 20000},
+    "6": {"name": "Shashlik", "price": 35000},
+    "7": {"name": "Somsa", "price": 12000},
+    "8": {"name": "Qovurma", "price": 28000},
+    "9": {"name": "Beshbarmoq", "price": 26000},
+    "10": {"name": "Tovuq go'shti", "price": 30000}
+}
+
+
